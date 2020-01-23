1c313e8ef007eeb222629e7f984066316c386ecc
Egor Burkov <e.burkov@samsung.com>
2020-01-23 11:28:05 +0300

    Batched prediction; improve subprocesses' shutdown on exceptions

--- exp/inference/inference_folder.py
+++ exp/inference/inference_folder.py
@@ -12,6 +12,7 @@ sys.path.append('./')
 
 # PyTorch
 import torch
+torch.set_num_threads(1)
 from torch.autograd import Variable
 import torch.nn.functional as F
 import torch.utils.data
@@ -22,6 +23,7 @@ torch.set_grad_enabled(False)
 import numpy as np
 from PIL import Image
 import cv2
+cv2.setNumThreads(0)
 from tqdm import tqdm
 
 # Custom imports
@@ -33,30 +35,20 @@ label_colours = [(0,0,0)
                 , (0,0,255), (51,170,221), (0,255,255), (85,255,170), (170,255,85), (255,255,0), (255,170,0)]
 
 
-def flip(x, dim):
-    indices = [slice(None)] * x.dim()
-    indices[dim] = torch.arange(x.size(dim) - 1, -1, -1,
-                                dtype=torch.long, device=x.device)
-    return x[tuple(indices)]
-
 def flip_cihp(tail_list):
-    '''
+    """
+        Swap channels in a probability map so that "left foot" becomes "right foot" etc.
 
-    :param tail_list: tail_list size is 1 x n_class x h x w
-    :return:
-    '''
-    # tail_list = tail_list[0]
-    tail_list_rev = [None] * 20
-    for xx in range(14):
-        tail_list_rev[xx] = tail_list[xx].unsqueeze(0)
-    tail_list_rev[14] = tail_list[15].unsqueeze(0)
-    tail_list_rev[15] = tail_list[14].unsqueeze(0)
-    tail_list_rev[16] = tail_list[17].unsqueeze(0)
-    tail_list_rev[17] = tail_list[16].unsqueeze(0)
-    tail_list_rev[18] = tail_list[19].unsqueeze(0)
-    tail_list_rev[19] = tail_list[18].unsqueeze(0)
-    return torch.cat(tail_list_rev,dim=0)
-
+        tail_list: (B x n_class x h x w)
+    """
+    return torch.cat((
+        tail_list[:, :14],
+        tail_list[:, 14:15],
+        tail_list[:, 15:16],
+        tail_list[:, 17:18],
+        tail_list[:, 16:17],
+        tail_list[:, 19:20],
+        tail_list[:, 18:19]), dim=1)
 
 def decode_labels(mask, num_images=1, num_classes=20):
     """Decode batch of segmentation masks.
@@ -93,162 +85,9 @@ def img_transform(img, transform):
     sample = transform(sample)
     return sample['image']
 
-def inference(net, img_paths, output_path, scale_list=[1.0, 0.5, 0.75, 1.25, 1.5, 1.75], use_gpu=True, save_extra_data=False):
-    # 1.0 should always go first
-    try:
-        scale_list.remove(1.0)
-    except ValueError:
-        pass
-    scale_list.insert(0, 1.0)
-
-    # Compute the longest common prefix to determine output paths
-    common_prefix = os.path.commonpath(img_paths)
-
-    # adj
-    adj2_ = torch.from_numpy(graph.cihp2pascal_nlp_adj).float()
-    adj2_test = adj2_.unsqueeze(0).unsqueeze(0).expand(1, 1, 7, 20).cuda().transpose(2, 3)
-
-    adj1_ = Variable(torch.from_numpy(graph.preprocess_adj(graph.pascal_graph)).float())
-    adj3_test = adj1_.unsqueeze(0).unsqueeze(0).expand(1, 1, 7, 7).cuda()
-
-    cihp_adj = graph.preprocess_adj(graph.cihp_graph)
-    adj3_ = Variable(torch.from_numpy(cihp_adj).float())
-    adj1_test = adj3_.unsqueeze(0).unsqueeze(0).expand(1, 1, 20, 20).cuda()
-
-    # One testing epoch
-    net.eval()
-
-    class InferenceDataset(torch.utils.data.Dataset):
-        def __init__(self, img_paths, scale_list):
-            self.img_paths = img_paths
-            self.scale_list = scale_list
-
-        def __len__(self):
-            return len(self.img_paths)
-
-        def __getitem__(self, idx):
-            image_path = self.img_paths[idx]
-            img = read_img(image_path)
-            
-            original_size = torch.tensor(img.size) # to make `default_collate` happy
-            img = img.resize((256, 256))
-
-            img_flipped = img_transform(img, tr.HorizontalFlip_only_img())
-
-            retval, retval_flipped = [], []
-            for scale in self.scale_list:
-                transform = transforms.Compose([
-                    tr.Scale_only_img(scale),
-                    tr.Normalize_xception_tf_only_img(),
-                    tr.ToTensor_only_img()])
-
-                retval.append(img_transform(img, transform))
-                retval_flipped.append(img_transform(img_flipped, transform))
-
-            # `str()` because `default_collate` doesn't like `Path`
-            return retval, retval_flipped, str(image_path), original_size
-
-    dataset = InferenceDataset(img_paths, scale_list)
-    dataloader = torch.utils.data.DataLoader(dataset, num_workers=1)
-
-    # Initialize saver processes
-    class ConstrainedTaskPool:
-        def __init__(self, num_processes=1, max_tasks=100):
-            import multiprocessing
-
-            self.num_processes = num_processes
-            self.task_queue = multiprocessing.Queue(maxsize=max_tasks)
-
-            def worker_function(task_queue):
-                for function, args in iter(task_queue.get, 'STOP'):
-                    function(*args)
-
-            for _ in range(self.num_processes):
-                multiprocessing.Process(target=worker_function, args=(self.task_queue,)).start()
-
-        def put_async(self, function, *args):
-            self.task_queue.put((function, args))
-
-        def __del__(self):
-            for _ in range(self.num_processes):
-                self.task_queue.put('STOP')
-
-    background_saver = ConstrainedTaskPool(num_processes=4, max_tasks=3000)
-
-    def save_image_async(image, path):
-        background_saver.put_async(cv2.imwrite, str(path), image)
-
-    exec_times = []
-    for sample_idx, (images, images_flipped, image_path, original_size) in enumerate(dataloader):
-        # `images`, `images_flipped`: list of length <number-of-scales>,
-        #   each element is a tensor of shape (<batch-size> x 3 x H_k x W_k);
-        # `image_paths`: tuple of length <batch-size> of str
-        # `original_size`: int tensor of shape (<batch-size> x 2)
-        if sample_idx % 100 == 0:
-            import datetime
-            print(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: {sample_idx} / {len(dataloader)}")
-
-        original_size = tuple(original_size[0].tolist())
-
-        image_path = Path(image_path[0])
-
-        start_time = timeit.default_timer()
-
-        for iii, (image, image_flipped) in enumerate(zip(images, images_flipped)):
-            inputs = torch.cat((image, image_flipped))
-            if iii == 0:
-                _, _, h, w = inputs.shape
-
-            if use_gpu:
-                inputs = inputs.cuda()
-
-            outputs = net.forward(inputs, adj1_test.cuda(), adj3_test.cuda(), adj2_test.cuda())
-            outputs = (outputs[0] + flip(flip_cihp(outputs[1]), dim=-1)) / 2
-            outputs = outputs.unsqueeze(0)
-
-            if iii > 0:
-                outputs = F.upsample(outputs, size=(h, w), mode='bilinear', align_corners=True)
-                outputs_final = outputs_final + outputs
-            else:
-                outputs_final = outputs
-
-        # outputs_final: `B x 20 x H x W`
-        end_time = timeit.default_timer()
-        exec_times.append(end_time - start_time)
-
-        # Actually write the outputs to disk
-        image_path = image_path.relative_to(common_prefix)
-
-        if save_extra_data:
-            predictions = torch.max(outputs_final, 1)[1]
-            results = predictions.cpu().numpy()
-
-            for output_folder in 'mask_gray', 'mask_color', 'segmented':
-                (output_path / output_folder / image_path.parent).mkdir(parents=True, exist_ok=True)
-
-            # saving grayscale mask image
-            save_image_async(results[0, :, :], output_path / 'mask_gray' / image_path.with_suffix('.png'))
-
-            # saving colored mask image
-            vis_res = decode_labels(results)
-            save_image_async(vis_res[0], output_path / 'mask_color' / image_path.with_suffix('.png'))
-
-            # saving segmented image with masked pixels drawn black
-            segmented_img = np.asarray(images[0][0] * 0.5 + 0.5) * (results[0, :, :] > 0).astype(np.float)[np.newaxis]
-            save_image_async(
-                segmented_img.transpose(1,2,0) * 255,
-                output_path / 'segmented' / image_path.with_suffix('.png'))
-        else:
-            background_probability = 1.0 - outputs_final.softmax(1)[:, 0] # `B x H x W`
-            background_probability = (background_probability * 255).round().byte().cpu().numpy()
-
-            (output_path / image_path.parent).mkdir(parents=True, exist_ok=True)
-            save_image_async(
-                cv2.resize(background_probability[0, :, :], original_size),
-                output_path / image_path.with_suffix('.png'))
-
-    print('Average inference time:', np.mean(exec_times))
-
+def save_image(image, path):
+    path.parent.mkdir(parents=True, exist_ok=True)
+    cv2.imwrite(str(path), image)
 
 if __name__ == '__main__':
     '''argparse begin'''
@@ -270,39 +109,193 @@ if __name__ == '__main__':
                                                                                  hidden_layers=128,
                                                                                  source_classes=7, )
 
-    net.load_source_model(torch.load(opts.model_path))
-    net.cuda()
+    # Initialize saver processes
+    import multiprocessing
 
-    image_paths_list: List[Path]
-    
-    if opts.images_path.is_file():
-        print(f"`--images_path` is a file, reading it for a list of files...")
-        with open(opts.images_path, 'r') as f:
-            image_paths_list = sorted(Path(line.strip()) for line in f)
-    elif opts.images_path.is_dir():
-        print(f"`--images_path` is a directory, recursively looking for images in it...")
+    class ConstrainedTaskPool:
+        def __init__(self, num_processes=1, max_tasks=100):
+            self.num_processes = num_processes
+            self.task_queue = multiprocessing.Queue(maxsize=max_tasks)
+
+        def __enter__(self):
+            def worker_function(task_queue):
+                for function, args in iter(task_queue.get, 'STOP'):
+                    function(*args)
+
+            for _ in range(self.num_processes):
+                multiprocessing.Process(target=worker_function, args=(self.task_queue,)).start()
+
+            return self
+
+        def __exit__(self, *args):
+            for _ in range(self.num_processes):
+                self.task_queue.put('STOP')
+
+        def put_async(self, function, *args):
+            self.task_queue.put((function, args))
+
+    with ConstrainedTaskPool(num_processes=4, max_tasks=6000) as background_saver:
+        def save_image_async(image, path):
+            background_saver.put_async(save_image, image, path)
+
+        net.load_source_model(torch.load(opts.model_path))
+        net.cuda()
+
+        # adj
+        adj2_ = torch.from_numpy(graph.cihp2pascal_nlp_adj).float()
+        adj2_test = adj2_.unsqueeze(0).unsqueeze(0).expand(1, 1, 7, 20).cuda().transpose(2, 3)
+
+        adj1_ = Variable(torch.from_numpy(graph.preprocess_adj(graph.pascal_graph)).float())
+        adj3_test = adj1_.unsqueeze(0).unsqueeze(0).expand(1, 1, 7, 7).cuda()
+
+        cihp_adj = graph.preprocess_adj(graph.cihp_graph)
+        adj3_ = Variable(torch.from_numpy(cihp_adj).float())
+        adj1_test = adj3_.unsqueeze(0).unsqueeze(0).expand(1, 1, 20, 20).cuda()
+
+        net.eval()
+
+        image_paths_list: List[Path]
         
-        def list_files_recursively(path, allowed_extensions):
-            retval = []
-            for child in path.iterdir():
-                if child.is_dir():
-                    retval += list_files_recursively(child, allowed_extensions)
-                elif child.suffix.lower() in allowed_extensions:
-                    retval.append(child)
+        if opts.images_path.is_file():
+            print(f"`--images_path` is a file, reading it for a list of files...")
+            with open(opts.images_path, 'r') as f:
+                image_paths_list = sorted(Path(line.strip()) for line in f)
+        elif opts.images_path.is_dir():
+            print(f"`--images_path` is a directory, recursively looking for images in it...")
+            
+            def list_files_recursively(path, allowed_extensions):
+                retval = []
+                for child in path.iterdir():
+                    if child.is_dir():
+                        retval += list_files_recursively(child, allowed_extensions)
+                    elif child.suffix.lower() in allowed_extensions:
+                        retval.append(child)
 
-            return retval
+                return retval
 
-        image_paths_list = sorted(list_files_recursively(opts.images_path, ('.png', '.jpg', '.jpeg')))
-    else:
-        raise FileNotFoundError(f"`--images_path` ('{opts.images_path}')")
+            image_paths_list = sorted(list_files_recursively(opts.images_path, ('.png', '.jpg', '.jpeg')))
+        else:
+            raise FileNotFoundError(f"`--images_path` ('{opts.images_path}')")
 
-    print(f"Found {len(image_paths_list)} images")
-    
-    tta = opts.tta
-    try:
-        tta = tta.split(',')
-        tta = list(map(float, tta))
-    except:
-        raise ValueError(f'tta must be a sequence of comma-separated float values such as "1.0,0.5,1.5". Got "{opts.tta}".')
+        print(f"Found {len(image_paths_list)} images")
+        
+        tta = opts.tta
+        try:
+            tta = tta.split(',')
+            tta = list(map(float, tta))
+        except:
+            raise ValueError(f'tta must be a sequence of comma-separated float values such as "1.0,0.5,1.5". Got "{opts.tta}".')
 
-    inference(net, image_paths_list,  opts.output_dir, use_gpu=True, scale_list=tta, save_extra_data=opts.save_extra_data)
+        scale_list = tta
+        # 1.0 should always go first
+        try:
+            scale_list.remove(1.0)
+        except ValueError:
+            pass
+        scale_list.insert(0, 1.0)
+
+        # Compute the longest common prefix to determine output paths
+        common_prefix = os.path.commonpath(image_paths_list)
+
+        class InferenceDataset(torch.utils.data.Dataset):
+            def __init__(self, img_paths, scale_list):
+                self.img_paths = img_paths
+                self.scale_list = scale_list
+
+            def __len__(self):
+                return len(self.img_paths)
+
+            def __getitem__(self, idx):
+                image_path = self.img_paths[idx]
+                img = read_img(image_path)
+                
+                original_size = torch.tensor(img.size) # to make `default_collate` happy
+                img = img.resize((256, 256))
+
+                img_flipped = img_transform(img, tr.HorizontalFlip_only_img())
+
+                retval, retval_flipped = [], []
+                for scale in self.scale_list:
+                    transform = transforms.Compose([
+                        tr.Scale_only_img(scale),
+                        tr.Normalize_xception_tf_only_img(),
+                        tr.ToTensor_only_img()])
+
+                    retval.append(img_transform(img, transform))
+                    retval_flipped.append(img_transform(img_flipped, transform))
+
+                # `str()` because `default_collate` doesn't like `Path`
+                return retval, retval_flipped, str(image_path), original_size
+
+        dataset = InferenceDataset(image_paths_list, scale_list)
+        dataloader = torch.utils.data.DataLoader(dataset, batch_size=14, num_workers=2)
+
+        exec_times = []
+        for sample_idx, (images, images_flipped, image_paths, original_sizes) in enumerate(dataloader):
+            # `images`, `images_flipped`: list of length <number-of-scales>,
+            #   each element is a tensor of shape (<batch-size> x 3 x H_k x W_k);
+            # `image_paths`: tuple of length <batch-size> of str
+            # `original_size`: int tensor of shape (<batch-size> x 2)
+            if sample_idx % 10 == 0:
+                import datetime
+                print(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}: {sample_idx} / {len(dataloader)}")
+
+            original_sizes = [tuple(original_size.tolist()) for original_size in original_sizes]
+            image_paths = [Path(x).relative_to(common_prefix) for x in image_paths]
+
+            batch_size = len(images[0])
+
+            start_time = timeit.default_timer()
+
+            for iii, (image, image_flipped) in enumerate(zip(images, images_flipped)):
+                inputs = torch.cat((image, image_flipped))
+                if iii == 0:
+                    _, _, h, w = inputs.shape
+
+                inputs = inputs.cuda()
+
+                outputs = net.forward(inputs, adj1_test.cuda(), adj3_test.cuda(), adj2_test.cuda())
+                outputs = (outputs[:batch_size] + torch.flip(flip_cihp(outputs[batch_size:]), dims=[-1,])) / 2
+
+                if iii > 0:
+                    outputs = F.upsample(outputs, size=(h, w), mode='bilinear', align_corners=True)
+                    outputs_final = outputs_final + outputs
+                else:
+                    outputs_final = outputs
+
+            # outputs_final: `B x 20 x H x W`
+            end_time = timeit.default_timer()
+            exec_times.append(end_time - start_time)
+
+            # Actually write the outputs to disk
+            if opts.save_extra_data:
+                predictions = torch.max(outputs_final, 1)[1]
+                results = predictions.cpu().numpy()
+                vis_results = decode_labels(results)
+
+                for input_image_1xScale, vis_result, result, image_path \
+                    in zip(images[0], vis_results, results, image_paths):
+
+                    # saving grayscale mask image
+                    save_image_async(result, opts.output_dir / 'mask_gray' / image_path.with_suffix('.png'))
+
+                    # saving colored mask image
+                    save_image_async(vis_result, opts.output_dir / 'mask_color' / image_path.with_suffix('.png'))
+
+                    # saving segmented image with masked pixels drawn black
+                    segmented_img = np.asarray(input_image_1xScale * 0.5 + 0.5) * (result > 0).astype(np.float)[np.newaxis]
+                    save_image_async(
+                        segmented_img.transpose(1,2,0) * 255,
+                        opts.output_dir / 'segmented' / image_path.with_suffix('.png'))
+            else:
+                background_probability = 1.0 - outputs_final.softmax(1)[:, 0] # `B x H x W`
+                background_probability = (background_probability * 255).round().byte().cpu().numpy()
+
+                for background_probability_single_sample, image_path, original_size \
+                    in zip(background_probability, image_paths, original_sizes):
+
+                    save_image_async(
+                        cv2.resize(background_probability_single_sample, original_size),
+                        opts.output_dir / image_path.with_suffix('.png'))
+
+    print('Average inference time:', np.mean(exec_times))
